80bb3b4fae5fa622708469e0197b563595df5cd7
alexgshepherd <alexander.shepherd3@gmail.com>
2020-07-02 09:45:27 -0400

    Update LmeSocketClient.java

--- complete/src/main/java/org/theenergymashuplab/cts/LmeSocketClient.java
+++ complete/src/main/java/org/theenergymashuplab/cts/LmeSocketClient.java
@@ -1,13 +1,36 @@
+/*
+ * Copyright 2019-2020 The Energy Mashup Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.theenergymashuplab.cts;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -35,6 +58,7 @@ public class LmeSocketClient	extends Thread {
 
 	private Socket clientSocket;
 	private PrintWriter out;
+	private static InputStreamReader inStream;
 	private BufferedReader in;
 	
     // Socket Server in LME for CreateTransaction
@@ -46,14 +70,15 @@ public class LmeSocketClient	extends Thread {
 	private static String ip = "127.0.0.1";
 	
 	// queueToMarket is for processed MarketCreateTenderPayload objects
+	private static BlockingQueue<String> queueToMarket = new ArrayBlockingQueue(20);
 	
-	// queueFromLme in LME for EiCreateTender payloads
-	// in LmeRestController.queueFromLme
-	//	private static BlockingQueue<EiCreateTenderPayload> queueFromLme = new ArrayBlockingQueue(20);
-		
+	//	TODO better document queues on parity and CTS side
+	
+	private static String driverLine;	// input line to drive to Market - json encoding
+	private static String s;
+	private static int ITERATIONS = 27;
+	
 	public LmeSocketClient()	{
-    	System.err.println("LmeSocketClient: 0 param constructor port " +
-    			port + " ip " + ip + " " + Thread.currentThread().getName());
 	}
 
 	
@@ -64,10 +89,9 @@ public class LmeSocketClient	extends Thread {
 		EiTender tender;
 		MarketCreateTenderPayload toJson;
 		String jsonString = null;	// for JSON string
-		
-//		logger.info("LmeSocketClient.run " + Thread.currentThread().getName() + " port " + port + " ip " + ip);
-// 		System.err.println("LmeSocketClient.run() port " + port + " ip " + ip +
-// 				" '" + Thread.currentThread().getName() + "'");
+
+		logger.trace("LmeSocketClient.run " + Thread.currentThread().getName() +
+					" port " + port + " ip " + ip);
 		
 		try {
 				clientSocket = new Socket(ip, port);
@@ -81,11 +105,12 @@ public class LmeSocketClient	extends Thread {
 		}
 		  
 		while(true) {
-//			  logger.debug("SocketClient while loop head. queueFromLme size " + LmeRestController.queueFromLme.size());
+			  logger.debug("SocketClient while loop head. queueFromLme size " +
+				LmeRestController.queueFromLme.size());
 			  try {
 				create = LmeRestController.queueFromLme.take();
-//				logger.debug("run() took from queueFromLme: size now " + LmeRestController.queueFromLme.size() +
-//						" " + create.getTender().toString());
+				logger.debug("run() took from queueFromLme: size now " + LmeRestController.queueFromLme.size() +
+						" " + create.getTender().toString());
 				tender = create.getTender();
 				toJson = new MarketCreateTenderPayload(
 							tender.getSide(),
@@ -100,9 +125,9 @@ public class LmeSocketClient	extends Thread {
 							
 				// convert to a JSON string and write to socket
 				jsonString = mapper.writeValueAsString(toJson);
-//				logger.debug("run() before send of json string " + jsonString);
+				logger.trace("run() before send of json string " + jsonString);
 				out.println(jsonString);			
-//				logger.info("LME Socket Client after sending parity json string " + jsonString);
+				logger.trace("LME Socket Client after sending parity json string " + jsonString);
 				
 			} catch (InterruptedException e) {
 				System.err.println("queueFromLme.take interrupted" + e.getMessage());
