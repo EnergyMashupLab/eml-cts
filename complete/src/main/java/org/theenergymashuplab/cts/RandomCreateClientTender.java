5e22ad781d56c34c04fb060e2cb8c6707bfe870e
alexgshepherd <alexander.shepherd3@gmail.com>
2020-07-02 09:46:43 -0400

    Update RandomCreateClientTender.java

--- complete/src/main/java/org/theenergymashuplab/cts/RandomCreateClientTender.java
+++ complete/src/main/java/org/theenergymashuplab/cts/RandomCreateClientTender.java
@@ -1,11 +1,40 @@
+/*
+ * Copyright 2019-2020 The Energy Mashup Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.theenergymashuplab.cts;
 
-import java.time.Instant;
-import java.util.Random;
+import java.time.*;
 
+import java.time.Instant;
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+/*
+ * 	Utility routine to generate random CreateClientTenderPayloads
+ * 	TODO move to utilities package
+ */
 
 public class RandomCreateClientTender {
 	
@@ -77,6 +106,7 @@ public class RandomCreateClientTender {
 		
 		startTime = instants[rand.nextInt(24)];
 		price = 10*(rand.nextInt(29) + 1);	//random price from 1..30 cents
+		Instant exp = dtStart.plusSeconds(60*60*11);	// DEBUG 11 hours after dtStart
 
 		SideType side = SideType.BUY;
 		if (rand.nextInt(100) > 50)	{
