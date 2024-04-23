ffa10cd4b819da96ddfe1776c0c41334419cb883
Nick Perri <nap3@njit.edu>
2024-04-22 20:27:45 -0400

    Changed a comment

--- complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiRequestPositionPayload.java
+++ complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiRequestPositionPayload.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//TODO: Create MarketIdType class, add comments
+//TODO: Add comments
  package org.theenergymashuplab.cts.controller.payloads;
 
 import org.theenergymashuplab.cts.Interval;
