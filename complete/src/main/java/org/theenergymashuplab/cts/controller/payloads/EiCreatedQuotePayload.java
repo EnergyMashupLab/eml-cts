2bf670774c5bd617bcba579d00e18c3c4880d123
jackr276 <jack_m_robbins@me.com>
2024-10-05 15:18:12 -0400

    Added setters, getters, constructors and toStrings for EiCreatedQuotePayload and EiCreateQuotePayload

--- complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiCreatedQuotePayload.java
+++ complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiCreatedQuotePayload.java
@@ -1,5 +1,7 @@
 package org.theenergymashuplab.cts.controller.payloads;
 
+import java.sql.Ref;
+
 import org.theenergymashuplab.cts.ActorIdType;
 import org.theenergymashuplab.cts.EiResponseType;
 import org.theenergymashuplab.cts.MarketOrderIdType;
@@ -23,4 +25,73 @@ public class EiCreatedQuotePayload {
 	public EiCreatedQuotePayload(){
 
 	}
+
+	public EiCreatedQuotePayload(ActorIdType counterPartyId, RefIdType inResponseTo, MarketOrderIdType marketQuoteId, ActorIdType partyId, TenderIdType quoteId, EiResponseType response){
+		this.counterPartyId = counterPartyId;
+		this.inResponseTo = inResponseTo;
+		this.marketQuoteId = marketQuoteId;
+		this.partyId = partyId;
+		this.quoteId = quoteId;
+		this.response = response;
+	}
+
+	public ActorIdType getCounterPartyId(){
+		return this.counterPartyId;
+	}
+
+	public void setCounterPartyId(ActorIdType counterPartyId){
+		this.counterPartyId = counterPartyId;
+	}
+
+	public RefIdType getInResponseTo(){
+		return this.inResponseTo;
+	}
+
+	public void setInResponseTo(RefIdType inResponseTo){
+		this.inResponseTo = inResponseTo;
+	}
+
+	public MarketOrderIdType getMarketQuoteId(){
+		return this.marketQuoteId;
+	}
+
+	public void setMarketQuoteId(MarketOrderIdType marketQuoteId){
+		this.marketQuoteId = marketQuoteId;
+	}
+
+	public ActorIdType getPartyId(){
+		return this.partyId;
+	}
+
+	public void setPartyId(ActorIdType partyId){
+		this.partyId = partyId;
+	}
+
+	public TenderIdType getQuoteId(){
+		return this.quoteId;
+	}
+
+	public void setQuoteId(TenderIdType quoteId){
+		this.quoteId = quoteId;
+	}
+
+	public EiResponseType getResponse(){
+		return this.response;
+	}
+
+	public void setResponse(EiResponseType response){
+		this.response = response;
+	}
+
+	@Override
+	public String toString(){
+		return "EiCreatedQuotePayload [" +
+				"counterPartyId=" + this.counterPartyId.toString() +
+				", inResonseTo=" + this.inResponseTo.toString() +
+				", marketQuoteId=" + this.marketQuoteId.toString() +
+				", partyId=" + this.partyId.toString() +
+				", quoteId=" + this.quoteId.toString() +
+				", response=" + this.response.toString() + "]";
+	}
+
 }
