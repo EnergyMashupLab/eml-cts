0c61a16ad8bedcf657343ed29b67c5eaec3a6e17
jackr276 <jack_m_robbins@me.com>
2024-10-04 20:53:50 -0400

    Implement setters, getters, constructors and toStrings for EiCancelQuotePayload, EiCanceledQuotePayload, EiAcceptQuotePayload and EiAcceptedQuotePayload

--- complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiCancelQuotePayload.java
+++ complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiCancelQuotePayload.java
@@ -21,4 +21,62 @@ public class EiCancelQuotePayload {
 	public EiCancelQuotePayload(){
 
 	}
+
+	public EiCancelQuotePayload(ActorIdType counterPartyId, MarketOrderIdType marketQuoteIds, ActorIdType partyId, TenderIdType quoteIds, RefIdType requestId){
+		this.counterPartyId = counterPartyId;
+		this.marketQuoteIds = marketQuoteIds;
+		this.partyId = partyId;
+		this.quoteIds = quoteIds;
+		this.requestId = requestId;
+	}
+
+	public ActorIdType getCounterPartyId(){
+		return this.counterPartyId;
+	}
+
+	public void setCounterPartyId(ActorIdType counterPartyId){
+		this.counterPartyId = counterPartyId;
+	}
+
+	public MarketOrderIdType getMarketQuoteIds(){
+		return this.marketQuoteIds;
+	}
+
+	public void setMarketQuoteIds(MarketOrderIdType marketQuoteIds){
+		this.marketQuoteIds = marketQuoteIds;
+	}
+
+	public ActorIdType getPartyId(){
+		return this.getPartyId();
+	}
+
+	public void setPartyId(ActorIdType partyId){
+		this.partyId = partyId;
+	}
+
+	public TenderIdType getQuoteIds(){
+		return this.quoteIds;
+	}
+
+	public void setQuoteIds(TenderIdType quoteIds){
+		this.quoteIds = quoteIds;
+	}
+
+	public RefIdType getRequestId(){
+		return this.requestId;
+	}
+
+	public void setRequestId(RefIdType requestId){
+		this.requestId = requestId;
+	}
+	
+	@Override
+	public String toString(){
+		return "EiCancelQuotePayload [" +
+				"counterPartyId=" + this.counterPartyId.toString() +
+				", marketQuoteIds=" + this.marketQuoteIds.toString() +
+				", partyId=" + this.partyId.toString() +
+				", quoteIds=" + this.quoteIds.toString() +
+				", requestId=" + this.requestId.toString() + "]";
+	}
 }
