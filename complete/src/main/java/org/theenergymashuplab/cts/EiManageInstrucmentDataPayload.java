1933555286579b4fdab29a536deaee800857faf3
root <andygyakobo@gmail.com>
2024-10-08 02:27:44 -0400

    Setup the EiManageInstrucmentDataPayload

---
+++ complete/src/main/java/org/theenergymashuplab/cts/EiManageInstrucmentDataPayload.java
@@ -0,0 +1,37 @@
+package org.theenergymashuplab.cts;
+
+public class EiManageInstrumentDataPayload {
+
+    private String instrumentId;
+    private String instrumentType;
+    private double instrumentValue;
+
+    // Constructor
+    public EiManageInstrumentDataPayload(String instrumentId, String instrumentType, double instrumentValue) {
+        this.instrumentId       = instrumentId;
+        this.instrumentType     = instrumentType;
+        this.instrumentValue    = instrumentValue;
+    }
+
+    // Getter(s)
+    public String getInstrumentId() {
+        return instrumentId;
+    }
+    public String getInstrumentType() {
+        return instrumentType;
+    }
+    public double getInstrumentValue() {
+        return instrumentValue;
+    }
+
+    // Setter(s)
+    public void setInstrumentID(String instrumentId) {
+        this.instrumentId = instrumentId;
+    } 
+    public void setInstrumentType(String instrumentType) {
+        this.instrumentType = instrumentType;
+    } 
+    public void setInstrumentValue(String instrumentValue) {
+        this.instrumentValue = instrumentValue;
+    } 
+}
