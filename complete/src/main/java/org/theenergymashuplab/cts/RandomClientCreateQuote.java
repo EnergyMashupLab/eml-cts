f65e3e29c85a8ed101e54712f0939adc35185521
dkhan21 <dkhann17@gmail.com>
2024-12-03 11:49:43 -0500

    Random Quote update

--- complete/src/main/java/org/theenergymashuplab/cts/RandomClientCreateQuote.java
+++ complete/src/main/java/org/theenergymashuplab/cts/RandomClientCreateQuote.java
@@ -14,20 +14,65 @@ public class RandomClientCreateQuote {
 
         //Intialize all the field that need to create for each Quote Object
 
-        //Create the payload that hold the quotes
+        //1 Create the payload that hold the quotes
         ClientCreateQuotePayload tempClientPayload;
         ClientCreatedQuotePayload resultPayload;
+        tempClientPayload = randomQuote();
+
+        // 2 Create TenderDetail
+        TenderDetail tenderDetail;
+        tenderDetail = new TenderIntervalDetail(
+                tempClientPayload.getInterval(),
+                tempClientPayload.getPrice(),
+                tempClientPayload.getQuantity()
+        );
+
+        // 3 Create EiQuoteType: creating a quote
+        EiQuoteType quote;
+        quote = new EiQuoteType(
+                //The expire time is given to us by the client
+                tempClientPayload.getBridgeExpireTime().asInstant(),
+                //This should ALWAYS be sell
+                tempClientPayload.getSide(),
+                //Stuff in the interval tender
+                tenderDetail
+        );
+
+        //Assign configuration settings
+
+
+
+
 
     }
 
-    //This method will generate each quote and we will assign the value to the
+    //This method will generate each quote and we will assign the value to the Quote Payload
 
     public ClientCreateQuotePayload randomQuote(){
-
+        Random ran  = new Random();
         ClientCreateQuotePayload tempQuote;
 
-        return
+        //Assign random quantity
+        int quantity = 50 + ran.nextInt(50);
+
+        //Assign Price
+        long price = 120; // 12 cents
+        price = 10*(ran.nextInt(29) + 1);
+
+        // Assign side
+        SideType side = SideType.SELL;
+        if(ran.nextInt(100) < 50 ){
+            side =  SideType.BUY;
+        }
+
+        tempQuote = new ClientCreateQuotePayload(side, price, quantity);
+
+        return tempQuote;
     }
+
+    // Create a EiquoteType method
+
+
 
 
 
