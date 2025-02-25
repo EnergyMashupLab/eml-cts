b52ef1456ba40fb616e24704cf07328f3228780b
Jay Santamaria <js288@njit.edu>
2025-02-25 16:02:32 -0500

    Defined EiResponseType getters, setters, toString methods

--- complete/src/main/java/org/theenergymashuplab/cts/EiResponseType.java
+++ complete/src/main/java/org/theenergymashuplab/cts/EiResponseType.java
@@ -16,12 +16,93 @@
 
 package org.theenergymashuplab.cts;
 
+/**
+ * @author crossover
+ * @version 1.0
+ * @created 16-Feb-2025 12:43:48 PM
+ */
+
 /* 
  * TBD - integration using NIST_CTS EiResponseType, 
  * EiResponseModel, EiResponseRepository
- * 
- * TODO
  */
 public class EiResponseType {
+
+	public InstantType createdDateTime;
+	public RefIdType inResponseTo;
+	public long responseCode;
+	public String responseDescription;
+	public ResponseDetailType responseDetail;
+
+	public EiResponseType() {
+		this.createdDateTime = new InstantType();
+		this.inResponseTo = new RefIdType();
+		this.responseCode = 0;
+		this.responseDescription = "";
+		this.responseDetail = ResponseDetailType.UNSPECIFIED;
+		
+		
+	}
+
+	public EiResponseType(InstantType createdDateTime, RefIdType inResponseTo, long responseCode,
+			String responseDescription, ResponseDetailType responseDetail) {
+		this.createdDateTime = createdDateTime;
+		this.inResponseTo = inResponseTo;
+		this.responseCode = responseCode;
+		this.responseDescription = responseDescription;
+		this.responseDetail = responseDetail;
+	}
+
+	public InstantType getCreatedDateTime() {
+		return createdDateTime;
+	}
+
+	public void setCreatedDateTime(InstantType createdDateTime) {
+		this.createdDateTime = createdDateTime;
+	}
+
+	public RefIdType getInResponseTo() {
+		return inResponseTo;
+	}
+
+	public void setInResponseTo(RefIdType inResponseTo) {
+		this.inResponseTo = inResponseTo;
+	}
+
+	public long getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseCode(long responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	public String getResponseDescription() {
+		return responseDescription;
+	}
+
+	public void setResponseDescription(String responseDescription) {
+		this.responseDescription = responseDescription;
+	}
+
+	public ResponseDetailType getResponseDetail() {
+		return responseDetail;
+	}
+
+	public void setResponseDetail(ResponseDetailType responseDetail) {
+		this.responseDetail = responseDetail;
+	}
+
+	@Override
+	public String toString() {
+	    return "EiResponseType{" +
+	           "createdDateTime=" + createdDateTime +
+	           ", inResponseTo=" + inResponseTo +
+	           ", responseCode=" + responseCode +
+	           ", responseDescription='" + responseDescription + '\'' +
+	           ", responseDetail=" + responseDetail +
+	           '}';
+	}
+
 
 }
