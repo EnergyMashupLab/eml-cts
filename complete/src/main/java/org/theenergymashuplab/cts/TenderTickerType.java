0415268c826f91a710860c0cd2f554e8eaddf617
Ayman Shahin <ayman.s.shahin122@gmail.com>
2025-02-26 17:50:02 -0500

    Add creation date

--- complete/src/main/java/org/theenergymashuplab/cts/TenderTickerType.java
+++ complete/src/main/java/org/theenergymashuplab/cts/TenderTickerType.java
@@ -5,7 +5,7 @@ import org.theenergymashuplab.cts.controller.payloads.TickerPayloadBase;
 /**
  * @author crossover
  * @version 1.0
- * @created 28-Sep-2024 8:41:42 PM
+ * @created 18-Feb-2025 11:30:37 AM
  */
 public class TenderTickerType extends TickerPayloadBase {
 
