3844d2d5bd4b3cdd0939dfb7fdf29d3a30b62711
tushiroda <tu34@njit.edu>
2025-02-26 11:49:57 -0500

    Formatted EiManageTickerSubscriptionPayload class

--- complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiManageTickerSubscriptionPayload.java
+++ complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiManageTickerSubscriptionPayload.java
@@ -11,7 +11,7 @@ public class EiManageTickerSubscriptionPayload extends EiSubscriptionRequestType
 
 	public TickerType tickerType;
 
-	public EiManageTickerSubscriptionPayload(){
+	public EiManageTickerSubscriptionPayload() {
 
 	}
 
@@ -19,11 +19,12 @@ public class EiManageTickerSubscriptionPayload extends EiSubscriptionRequestType
 		this.tickerType = tickerType;
 	}
 
-	public EiManageTickerSubscriptionPayload(MarketIdType marketId, int segmentId, SubscriptionActionType subscriptionActionRequested, RefIdType subscriptionRequestId, TickerType tickerType, ActorIdType partyId) {
+	public EiManageTickerSubscriptionPayload(MarketIdType marketId, int segmentId,
+			SubscriptionActionType subscriptionActionRequested, RefIdType subscriptionRequestId, TickerType tickerType,
+			ActorIdType partyId) {
 		super(marketId, segmentId, subscriptionActionRequested, subscriptionRequestId, partyId);
 		this.tickerType = tickerType;
 	}
-
 
 	public TickerType getTickerType() {
 		return tickerType;
