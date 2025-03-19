2d9b16e16a4b8d96b6609a6f13f21ec804c0eab0
tushiroda <tu34@njit.edu>
2025-03-19 17:38:25 -0400

    Updated EiRfqType.java file

--- complete/src/main/java/org/theenergymashuplab/cts/EiRfqType.java
+++ complete/src/main/java/org/theenergymashuplab/cts/EiRfqType.java
@@ -1,11 +1,21 @@
+/*
+ * Copyright 2019-2025 The Energy Mashup Lab
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.theenergymashuplab.cts;
 
-
-/**
- * @author crossover
- * @version 1.0
- * @created 28-Sep-2024 8:41:40 PM
- */
 public class EiRfqType extends TenderBase {
 
 	public IntervalType boundingInterval;
@@ -15,11 +25,12 @@ public class EiRfqType extends TenderBase {
 	public RfqIdType rfqId;
 	public boolean tradeableOnlyResponse;
 
-	public EiRfqType(){
+	public EiRfqType() {
 
 	}
 
-	public EiRfqType(IntervalType boundingInterval, DurationType duration, MarketOrderIdType marketRfqId, boolean privateRfq, RfqIdType rfqId, boolean tradeableOnlyResponse){
+	public EiRfqType(IntervalType boundingInterval, DurationType duration, MarketOrderIdType marketRfqId,
+			boolean privateRfq, RfqIdType rfqId, boolean tradeableOnlyResponse) {
 		this.boundingInterval = boundingInterval;
 		this.duration = duration;
 		this.marketRfqId = marketRfqId;
@@ -27,63 +38,63 @@ public class EiRfqType extends TenderBase {
 		this.rfqId = rfqId;
 		this.tradeableOnlyResponse = tradeableOnlyResponse;
 	}
-	
-	public IntervalType getBoundingInterval(){
+
+	public IntervalType getBoundingInterval() {
 		return this.boundingInterval;
 	}
 
-	public void setBoundingInterval(IntervalType boundingInterval){
+	public void setBoundingInterval(IntervalType boundingInterval) {
 		this.boundingInterval = boundingInterval;
 	}
 
-	public DurationType getDuration(){
+	public DurationType getDuration() {
 		return this.duration;
 	}
 
-	public void setDuratoin(DurationType duration){
+	public void setDuratoin(DurationType duration) {
 		this.duration = duration;
 	}
 
-	public MarketOrderIdType getMarketRfqId(){
+	public MarketOrderIdType getMarketRfqId() {
 		return this.marketRfqId;
 	}
 
-	public void setMarketRfqId(MarketOrderIdType marketRfqId){
+	public void setMarketRfqId(MarketOrderIdType marketRfqId) {
 		this.marketRfqId = marketRfqId;
 	}
 
-	public boolean getPrivateRfq(){
+	public boolean getPrivateRfq() {
 		return this.privateRfq;
 	}
 
-	public void setPrivateRfq(boolean privateRfq){
+	public void setPrivateRfq(boolean privateRfq) {
 		this.privateRfq = privateRfq;
 	}
 
-	public RfqIdType getRfqId(){
+	public RfqIdType getRfqId() {
 		return this.rfqId;
 	}
 
-	public void setRfqId(RfqIdType rfqId){
+	public void setRfqId(RfqIdType rfqId) {
 		this.rfqId = rfqId;
 	}
 
-	public boolean getTradeableOnlyResponse(){
+	public boolean getTradeableOnlyResponse() {
 		return this.tradeableOnlyResponse;
 	}
 
-	public void setTradeableOnlyResponse(boolean tradeableOnlyResponse){
+	public void setTradeableOnlyResponse(boolean tradeableOnlyResponse) {
 		this.tradeableOnlyResponse = tradeableOnlyResponse;
 	}
 
 	@Override
-	public String toString(){
+	public String toString() {
 		return "EiRfqType [" +
-			"boundingInterval=" + this.boundingInterval.toString() +
-			"duration=" + this.duration.toString() +
-			"marketRfqId=" + this.marketRfqId.toString() +
-			"privateRfq=" + this.privateRfq +
-			"rfqId=" + this.rfqId.toString() +
-			"tradeableOnlyResponse=" + this.tradeableOnlyResponse + "]";
+				"boundingInterval=" + this.boundingInterval.toString() +
+				"duration=" + this.duration.toString() +
+				"marketRfqId=" + this.marketRfqId.toString() +
+				"privateRfq=" + this.privateRfq +
+				"rfqId=" + this.rfqId.toString() +
+				"tradeableOnlyResponse=" + this.tradeableOnlyResponse + "]";
 	}
 }
