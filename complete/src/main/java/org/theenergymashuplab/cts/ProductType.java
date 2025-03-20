adfbca0c89eb5be49482cdcdf5a4b57ebe9d0b7d
tushiroda <tu34@njit.edu>
2025-03-19 21:32:57 -0400

    Updated ProductType.java file

--- complete/src/main/java/org/theenergymashuplab/cts/ProductType.java
+++ complete/src/main/java/org/theenergymashuplab/cts/ProductType.java
@@ -1,39 +1,66 @@
+/*
+ * Copyright 2019-2025 The Energy Mashup Lab
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.theenergymashuplab.cts;
 
+import java.util.ArrayList;
 
-/**
- * HOW  -- the resource is packaged for trading
- * @author crossover
- * @version 1.0
- * @created 28-Sep-2024 8:41:41 PM
- */
 public class ProductType extends ResourceType {
 
 	public DurationType duration;
 	public int quantityScale;
+	public ArrayList<WarrantIdType> warrants;
 	public WarrantIdType warrantId;
 
-	public ProductType(){
+	public ProductType() {
 
 	}
 
-	public ProductType(DurationType duration, int quantityScale, WarrantIdType warrantId) {
+	public ProductType(DurationType duration, int quantityScale, ArrayList<WarrantIdType> warrants) {
 		this.duration = duration;
 		this.quantityScale = quantityScale;
-		this.warrantId = warrantId;
+		this.warrants = warrants;
+		this.warrantId = warrants.getFirst();
 	}
 
-	public DurationType getDuration() { return duration; }
+	public DurationType getDuration() {
+		return duration;
+	}
 
-	public void setDuration(DurationType duration) { this.duration = duration; }
+	public void setDuration(DurationType duration) {
+		this.duration = duration;
+	}
 
-	public int getQuantityScale() { return quantityScale; }
+	public int getQuantityScale() {
+		return quantityScale;
+	}
 
-	public void setQuantityScale(int quantityScale) { this.quantityScale = quantityScale; }
+	public void setQuantityScale(int quantityScale) {
+		this.quantityScale = quantityScale;
+	}
 
-	public WarrantIdType getWarrantId() { return warrantId; }
+	public WarrantIdType getWarrantId() {
+		// Previously, warrants was a single value, not an arraylist of WarrantIdType.
+		return warrantId;
+	}
 
-	public void setWarrantId(WarrantIdType warrantId) { this.warrantId = warrantId; }
+	public void setWarrantId(ArrayList<WarrantIdType> warrants) {
+		this.warrants = warrants;
+		this.warrantId = warrants.getFirst();
+	}
 
 	@Override
 	public String toString() {
