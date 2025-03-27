4bc5a1907fc86048799b1422582d8fd4c9d19afd
tushiroda <tu34@njit.edu>
2025-03-27 19:43:32 -0400

    Removed duplicate .java files and moved updated code to correct files

--- complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiManageSegmentReferenceDataPayload.java
+++ complete/src/main/java/org/theenergymashuplab/cts/controller/payloads/EiManageSegmentReferenceDataPayload.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2019-2025 The Energy Mashup Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package org.theenergymashuplab.cts.controller.payloads;
 
 import org.theenergymashuplab.cts.EiSubscriptionRequestType;
@@ -5,11 +18,15 @@ import org.theenergymashuplab.cts.EiSubscriptionRequestType;
 /**
  * @author crossover
  * @version 1.0
- * @created 28-Sep-2024 8:41:40 PM
+ * @created 26-Feb-2025 11:30:00 AM
  */
+
 public class EiManageSegmentReferenceDataPayload extends EiSubscriptionRequestType {
 
-	public EiManageSegmentReferenceDataPayload(){
-
+	@Override
+	public String toString() {
+		return "EiManageSegmentReferenceDataPayload {" + "marketId=" + marketId + ", segmentId=" + segmentId
+				+ ", subscriptionActionRequested=" + subscriptionActionRequested + ", subscriptionRequestId="
+				+ subscriptionRequestId + '}';
 	}
 }
