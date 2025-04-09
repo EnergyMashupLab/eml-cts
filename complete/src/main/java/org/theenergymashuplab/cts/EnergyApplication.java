a4a1f2718d518434cde59b481ba4bbdc6896d99b
William Cox <wtcox@coxsoftwarearchitects.com>
2025-04-08 21:02:27 -0400

    Update parameters so random tenders do not have negative prices.

--- complete/src/main/java/org/theenergymashuplab/cts/EnergyApplication.java
+++ complete/src/main/java/org/theenergymashuplab/cts/EnergyApplication.java
@@ -80,7 +80,7 @@ public class EnergyApplication {
 			 * RandomCreateClientTender(int hour, int howMany, int priceLower, int priceUpper,
 			 * 		int quantityLower, int quantityUpper, int segmentId)
 			 */
-			RandomCreateClientTender randomClientTender = new RandomCreateClientTender(13, 50, -50, 100, 10, 300, 3);
+			RandomCreateClientTender randomClientTender = new RandomCreateClientTender(13, 50, 0, 100, 10, 300, 3);
 			
 //			RandomCreateClientStreamTender randomClientStreamTender = new RandomCreateClientStreamTender(25,24,5,50,22,122);
 //			RandomCreateClientQuote randomClientQuote = new RandomCreateClientQuote(500);
