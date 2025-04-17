a1d3f3b7b14a8b5d27792d62fd99c35c317766b7
Jay Santamaria <js288@njit.edu>
2025-04-17 05:01:38 -0400

    Bug fixes/resolve merge conflicts

--- complete/src/main/java/org/theenergymashuplab/cts/EiResponseType.java
+++ complete/src/main/java/org/theenergymashuplab/cts/EiResponseType.java
@@ -16,79 +16,67 @@
 
 package org.theenergymashuplab.cts;
 
+import java.time.Instant;
+
+import org.theenergymashuplab.cts.generated_files.ResponseDetailType;
+
+/* 
+ * TBD - integration using NIST_CTS EiResponseType, 
+ * EiResponseModel, EiResponseRepository
+ * 
+ * TODO
+ */
 public class EiResponseType {
-	public InstantType createdDateTime;
-	public RefIdType inResponseTo;
-	public long responseCode;
-	public String responseDescription;
-	public ResponseDetailType responseDetail;
+    public Instant createdDateTime;
+    public RefIdType inResponseTo;
+    public long responseCode;
+    public String responseDescription;
+    public ResponseDetailType responseDetail;
 
-	public EiResponseType() {
-		this.createdDateTime = new InstantType();
-		this.inResponseTo = new RefIdType();
-		this.responseCode = 0;
-		this.responseDescription = "";
-		this.responseDetail = ResponseDetailType.UNSPECIFIED;
-	}
+    public EiResponseType() {
 
-	public EiResponseType(InstantType createdDateTime, RefIdType inResponseTo, long responseCode,
-			String responseDescription, ResponseDetailType responseDetail) {
-		this.createdDateTime = createdDateTime;
-		this.inResponseTo = inResponseTo;
-		this.responseCode = responseCode;
-		this.responseDescription = responseDescription;
-		this.responseDetail = responseDetail;
-	}
+    }
 
-	public InstantType getCreatedDateTime() {
-		return createdDateTime;
-	}
+    public Instant getCreatedDateTime() {
+        return createdDateTime;
+    }
 
-	public void setCreatedDateTime(InstantType createdDateTime) {
-		this.createdDateTime = createdDateTime;
-	}
+    public void setCreatedDateTime(Instant createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
 
-	public RefIdType getInResponseTo() {
-		return inResponseTo;
-	}
+    public RefIdType getInResponseTo() {
+        return inResponseTo;
+    }
 
-	public void setInResponseTo(RefIdType inResponseTo) {
-		this.inResponseTo = inResponseTo;
-	}
+    public void setInResponseTo(RefIdType inResponseTo) {
+        this.inResponseTo = inResponseTo;
+    }
 
-	public long getResponseCode() {
-		return responseCode;
-	}
+    public long getResponseCode() {
+        return responseCode;
+    }
 
-	public void setResponseCode(long responseCode) {
-		this.responseCode = responseCode;
-	}
+    public void setResponseCode(long responseCode) {
+        this.responseCode = responseCode;
+    }
 
-	public String getResponseDescription() {
-		return responseDescription;
-	}
+    public String getResponseDescription() {
+        return responseDescription;
+    }
 
-	public void setResponseDescription(String responseDescription) {
-		this.responseDescription = responseDescription;
-	}
+    public void setResponseDescription(String responseDescription) {
+        this.responseDescription = responseDescription;
+    }
 
-	public ResponseDetailType getResponseDetail() {
-		return responseDetail;
-	}
+    
+    public ResponseDetailType getResponseDetail() {
+        return responseDetail;
+    }
 
-	public void setResponseDetail(ResponseDetailType responseDetail) {
-		this.responseDetail = responseDetail;
-	}
+    public void setResponseDetail(ResponseDetailType responseDetail) {
+        this.responseDetail = responseDetail;
+    }
 
-	@Override
-	public String toString() {
-		return "EiResponseType{" +
-				"createdDateTime=" + createdDateTime +
-				", inResponseTo=" + inResponseTo +
-				", responseCode=" + responseCode +
-				", responseDescription='" + responseDescription + '\'' +
-				", responseDetail=" + responseDetail +
-				'}';
-	}
 
 }
